c312dd6c916b24af98c4b99d2f61d28ff680712b
Mykola Rostotskyi <1nick.rost@gmail.com>
2024-10-30 22:35:48 +0200

    Initial project

---
+++ OIG.Survey/Data/ApplicationUser.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace OIG.Survey.Data
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+}
